b2405ed686a36be501178241d5f7f1c76dc37c9b
AsherRizvi <asherhussain565@gmail.com>
2024-03-03 19:35:32 +0500

    Revert "Implement ll_has_cycle() in ll_cycle.c"
    This reverts commit 95a427c983b284af7455edaf4cf7fc8622bff380.

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -2,29 +2,5 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    if (head == NULL) {
-        // Empty list, no cycle
-        return 0;
-    }
-
-    // Initialize two pointers, slow and fast
-    node *slow_ptr = head;
-    node *fast_ptr = head;
-
-    while (fast_ptr != NULL && fast_ptr->next != NULL) {
-        // Advance fast_ptr by two nodes
-        fast_ptr = fast_ptr->next->next;
-
-        // Advance slow_ptr by one node
-        slow_ptr = slow_ptr->next;
-
-        // If fast_ptr and slow_ptr point to the same node, there is a cycle
-        if (fast_ptr == slow_ptr) {
-            return 1;  // Cycle detected
-        }
-    }
-
-    // If the loop terminates, there is no cycle
-    return 0;
+    /* TODO: Implement ll_has_cycle */
 }
-
